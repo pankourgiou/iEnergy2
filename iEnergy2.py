35292f212b60aaf4fd2074c85b57baf47475b886
L <60981745+pankourgiou@users.noreply.github.com>
2025-02-03 09:47:03 +0200

    Create iEnergy2.py

---
+++ iEnergy2.py
@@ -0,0 +1,2 @@
+#Again here we set c = 1 and x = 1 then we have c*E*x= E this is why there was so much of a disagreement on the initial Achille's code the cex part was a labyrinth to me!! 
+print("cex = E = mcmod2")
