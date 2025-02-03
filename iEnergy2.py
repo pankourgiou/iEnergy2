c20ef46dd159ef723ef66777ffbd26acb5a9214a
L <60981745+pankourgiou@users.noreply.github.com>
2025-02-03 09:53:33 +0200

    Update iEnergy2.py

--- iEnergy2.py
+++ iEnergy2.py
@@ -1,2 +1,6 @@
 #Again here we set c = 1 and x = 1 then we have c*E*x= E this is why there was so much of a disagreement on the initial Achille's code the cex part was a labyrinth to me!! 
 print("cex = E = mcmod2")
+#we can set oo = 1 then we have E = oo*gamma*mcmod2 or swift = prolog = 1 then E = gamma*swift*prolog*mcmod2 so let's print it!!
+print("E = oo*gamma*mcmod2")
+#or/and
+print(" E = gamma*swift*prolog*mcmod2")
